db6d34c85077f56e47bac27b36c016108f962fa9
Eddyshmis <jazzir03@gmail.com>
2023-09-07 21:39:56 -0600

    better

--- Main.py
+++ Main.py
@@ -20,11 +20,22 @@ start_bt = classes.Button((center_x - 400),(center_y - 250),start_bt_img,0.5)
 
 
 #transformed image
-background_image = pygame.image.load("cyberpunk-street-files\cyberpunk-street-files\Version 1\PNG\layers\\foreground.png").convert_alpha()
+foreground_image = pygame.image.load("cyberpunk-street-files\cyberpunk-street-files\Version 1\PNG\layers\\foreground.png").convert_alpha()
 # background_transform = pygame.transform.scale(background_image,(1000,100))
+back_buildings_image = pygame.image.load("cyberpunk-street-files\cyberpunk-street-files\Version 1\PNG\layers\\back-buildings.png").convert_alpha()
+far_buildings_image = pygame.image.load("cyberpunk-street-files\cyberpunk-street-files\Version 1\PNG\layers\\far-buildings.png").convert_alpha()
+foreground = classes.image(0,0,foreground_image,4.5)
+back_buildings = classes.image(0,0,back_buildings_image,4.5)
+far_buildings = classes.image(0,0,far_buildings_image,5)
 
-background = classes.image(0,0,background_image,4.5)
+background_music = pygame.mixer.Sound("cyberpunk-street-files\cyberpunk-street-files\music\cyberpunk-street.mp3")
+music_playing = False
 while True:
+    if not music_playing:
+        background_music.play(1)
+        background_music.set_volume(0.1)
+        music_playing = True
+        print("music")
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -35,7 +46,9 @@ while True:
         #play game
         screen.fill((0,0,0))
         # screen.blit(background_transform,(0,0))
-        background.draw(screen)
+        far_buildings.draw(screen)
+        back_buildings.draw(screen)
+        foreground.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.JOYBUTTONDOWN:
                 print(event)
